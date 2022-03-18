30493c2cadf9bb1df75bb404eb171c4c26ebca32
vadudduk <dvk7520000@gmail.com>
2022-03-18 14:17:05 +0530

    Authentiction done

--- src/main/java/com/springboot/Jenkins/WebSecurityConfig.java
+++ src/main/java/com/springboot/Jenkins/WebSecurityConfig.java
@@ -41,11 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	http.authorizeRequests()
-        .antMatchers("/").permitAll()
-        .antMatchers("/locations").hasAnyRole("USER", "ADMIN")
         .anyRequest().authenticated()
         .and()
-        .httpBasic()
-        .and();
+        .httpBasic();
     }
 }
