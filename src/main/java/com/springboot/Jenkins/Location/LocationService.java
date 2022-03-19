f377cf4ec2ed8a888d0b4455440123ffc9845b53
vadudduk <dvk7520000@gmail.com>
2022-03-19 19:44:27 +0530

    Updating the database table -- Done

--- src/main/java/com/springboot/Jenkins/Location/LocationService.java
+++ src/main/java/com/springboot/Jenkins/Location/LocationService.java
@@ -11,7 +11,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
-@PropertySource(value={"classpath:application.properties"})
+@PropertySource(value={"file:application.properties"})
 public class LocationService {
 	
 	   //database
