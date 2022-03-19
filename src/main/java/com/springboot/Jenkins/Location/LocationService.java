ec415e75076d8639284d9157a74ae4a4379095f7
vadudduk <dvk7520000@gmail.com>
2022-03-19 23:27:44 +0530

    Adding application properties values code

--- src/main/java/com/springboot/Jenkins/Location/LocationService.java
+++ src/main/java/com/springboot/Jenkins/Location/LocationService.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
+@Configuration
 @PropertySource("classpath:application.properties")
 public class LocationService {
 	
