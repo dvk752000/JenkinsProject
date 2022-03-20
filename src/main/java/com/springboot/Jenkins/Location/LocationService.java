06adf77e2c82f4097f76d4d3ba5eebca8bf0f2cb
vadudduk <dvk7520000@gmail.com>
2022-03-20 19:47:17 +0530

    Implementing jenkins parameters

--- src/main/java/com/springboot/Jenkins/Location/LocationService.java
+++ src/main/java/com/springboot/Jenkins/Location/LocationService.java
@@ -45,7 +45,7 @@ public class LocationService {
 		
 		Location locationFromDb = locationRepository.findById(id).get();
 		//System.out.println("setDbData has a value: " + jdbcUrl1);
-		System.out.println("getProperty returned a value: " + System.getProperty("DOCKERHUB_CREDENTIALS_USR"));;
+		System.out.println("getProperty returned a value: " + System.getProperty("hsqlSource"));;
 		//locationFromDb.setName(message);
 
 	    return locationRepository.save(locationFromDb);
